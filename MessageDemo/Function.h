9f309d773d52b7bd043af2a4ea7d578bbf2fea78
wend <794363716@qq.com>
2015-12-15 16:11:16 +0800

    1,输入三次后 会输出姓名label √ 2，位置没有控制好 √ 3，键盘的背景没有拉伸   √ 4，时间没有显示 √ 5，菊花没有显示 √ 6,空格应该 不 可以发送
    1，图片不对 √
    2，位置不对  √
    3，copy位置不对  √
    4，隐藏键盘的点击不对，需要点击任意地方隐藏  √
    5，键盘多屏幕适配  √
    
    6，时间的控件的大小随着文本的大小改变
    
    错误图标会复用和不定期消失
    图片在发送消息小的时候会没有角 √
    里面的界面位置，不要用数字，用宏 √
    错误图片显示复用问题，

---
+++ MessageDemo/Function.h
@@ -0,0 +1,13 @@
+//
+//  Function.h
+//  MessageDemo
+//
+//  Created by wend on 15/12/14.
+//  Copyright © 2015年 wufei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Function : NSObject
+
+@end
---
+++ MessageDemo/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  MessageDemo
+//
+//  Created by wend on 15/12/10.
+//  Copyright (c) 2015年 wufei. All rights reserved.
+//
+
+#ifndef MessageDemo_Header_h
+#define MessageDemo_Header_h
+
+
+#endif
